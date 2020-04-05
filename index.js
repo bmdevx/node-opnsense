bdf8fbb97729c4be93aa72cba9c91b032625ba6f
bmdevx <bryancamrn@gmail.com>
2020-04-04 23:21:39 -0600

    package update

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    OPNSenseClient: require('./src/opnsense-client'),
+    BaseClient: require('./src/opnsense-baseclient'),
+    diagnostics: require('./src/opnsense-diagnostics'),
+    Vnstat: require('./src/opnsense-vnstat')
+}
